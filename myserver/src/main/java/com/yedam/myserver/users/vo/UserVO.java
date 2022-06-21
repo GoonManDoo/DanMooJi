f039e388b28c85526042d0723518e57a3069d0c0
KYM <kym8439@gmail.com>
2022-06-21 19:12:28 +0900

    06-21 무비 학원커밋

--- myserver/src/main/java/com/yedam/myserver/users/vo/UserVO.java
+++ myserver/src/main/java/com/yedam/myserver/users/vo/UserVO.java
@@ -1,40 +1,80 @@
 package com.yedam.myserver.users.vo;
 
-public class UserVO {
-	
-	private String id;
-	private String password;
-	private String name;
-	private String role;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	@Override
-	public String toString() {
-		return "UserVO [id=" + id + ", password=" + password + ", name=" + name
-				+ ", role=" + role + "]";
-	}
-	
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserVO implements UserDetails {
+   
+   private String id;
+   private String password;
+   private String name;
+   private String role;
+   
+   public String getId() {
+      return id;
+   }
+   public void setId(String id) {
+      this.id = id;
+   }
+   public String getPassword() {
+      return password;
+   }
+   public void setPassword(String password) {
+      this.password = password;
+   }
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getRole() {
+      return role;
+   }
+   public void setRole(String role) {
+      this.role = role;
+   }
+   @Override
+   public String toString() {
+      return "UserVO [id=" + id + ", password=" + password + ", name=" + name
+            + ", role=" + role + "]";
+   }
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities() {
+      //권한 지정
+      List<GrantedAuthority> auth = new ArrayList<>();
+      auth.add(new SimpleGrantedAuthority("ROLE_" + this.role.toUpperCase()));
+      return null;
+   }
+   @Override
+   public String getUsername() {
+      // TODO Auto-generated method stub
+      return id;
+   }
+   @Override
+   public boolean isAccountNonExpired() {
+      // TODO Auto-generated method stub
+      return true;
+   }
+   @Override
+   public boolean isAccountNonLocked() {
+      // TODO Auto-generated method stub
+      return true;
+   }
+   @Override
+   public boolean isCredentialsNonExpired() {
+      // TODO Auto-generated method stub
+      return true;
+   }
+   @Override
+   public boolean isEnabled() {
+      // TODO Auto-generated method stub
+      return true;
+   }
+   
 }
